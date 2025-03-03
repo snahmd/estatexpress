50ccef086c8305c1fc8f85c34bb0a5f1226c860a
San <sanahmed7@gmail.com>
2025-03-04 00:39:08 +0100

    Implement sign-in screen with Google login UI and hide default headers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,5 +24,5 @@ export default function RootLayout() {
   if (!fontsLoaded) {
     return null;
   }
-  return <Stack />;
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,38 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView , Image, TouchableOpacity} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
+import images from '@/constants/images'
+import icons from '@/constants/icons'
+
+
+
 
 const SignIn = () => {
+    const handleLogin = () => {
+        console.log("Login with Google")
+    }
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className="bg-white h-full">
+        <ScrollView contentContainerClassName="h-full">
+            <Image source={images.onboarding} className="w-full h-4/6" resizeMode="contain"  />
+            <View className="px-10">
+                <Text className="text-black-300 text-2xl font-rubik-bold text-center">Welcome to EstateXpress {"\n"}
+                    <Text className="text-primary-300">Your Ideal Home</Text>
+                </Text>
+                
+                <Text className="text-black-200  text-lg mt-12 font-rubik-medium text-center">Login to  EstateXpress  with Google </Text>
+                <TouchableOpacity className="bg-primary-300 shadow-md shadow-zinc-300 rounded-full p-4 mt-10" onPress={handleLogin}>
+                    <View className="flex-row items-center justify-center gap-2">
+                        <Image source={icons.google} className="w-6 h-6" resizeMode="contain" />
+                        <Text className="text-white text-lg font-rubik-medium text-center">Login with Google</Text>
+                    </View>
+                </TouchableOpacity>
+                   
+              
+                
+            </View>
+        </ScrollView>
+    </SafeAreaView>
   )
 }
 
